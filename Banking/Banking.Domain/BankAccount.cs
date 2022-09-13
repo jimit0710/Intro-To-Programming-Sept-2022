40ea800ec8952bee171764d8b552241591b67610
Jimit Bhalavat <jimit_bhalavat@progressive.com>
2022-09-13 15:18:53 -0400

    Banking

---
+++ Banking/Banking.Domain/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace Banking.Domain;
+
+public class BankAccount
+{
+    private decimal _balance = 5000M;
+
+    public void Deposit(decimal amountToDeposit)
+    {
+        _balance += amountToDeposit;
+    }
+
+    public void Withdraw(decimal amountToWithdraw)
+    {
+        _balance -= amountToWithdraw;
+    }
+
+    public decimal GetBalance()
+    {
+        return _balance;
+    }
+}
---
+++ Banking/Banking.UnitTests/AccountDeposits.cs
@@ -0,0 +1,18 @@
+﻿using Banking.Domain;
+namespace Banking.UnitTests;
+
+public class AccountDeposits
+{
+    [Fact]
+    public void MNakingADepositIncreasesTheBalance()
+    {
+        // Given
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 10.15M;
+        // When
+        account.Deposit(amountToDeposit);
+        // Then
+        Assert.Equal(openingBalance + amountToDeposit, account.GetBalance());
+    }
+}
