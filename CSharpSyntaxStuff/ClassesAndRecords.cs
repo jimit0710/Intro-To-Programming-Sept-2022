48da902830e6603454c7b5fe2cda8fc3862a1f7f
Jimit Bhalavat <jimit_bhalavat@progressive.com>
2022-09-13 12:36:58 -0400

    Unit Testing

---
+++ CSharpSyntaxStuff/ClassesAndRecords.cs
@@ -0,0 +1,51 @@
+﻿namespace CSharpSyntaxStuff;
+
+public class ClassesAndRecords
+{
+    [Fact]
+    public void DoThings()
+    {
+        var myName = "Jimit";
+        myName = myName.ToUpper();
+        Assert.Equal("JIMIT", myName);
+    }
+
+    [Fact]
+    public void BowlingForImmutableTypes()
+    {
+        var game = new BowlingGame();
+        // add a bunch of games
+
+        var winner = game.GetWinner();
+        // Assert.Equal("Jimit", winner.Name);
+        // Assert.Equal(198, winner.Score);
+
+        var expectedWinner = new Bowler("Jimit", 198);
+        Assert.Equal(expectedWinner, winner);
+        Assert.Equal("Jimit", winner.Name);
+    }
+}
+
+public record struct Point(int x, int y);
+
+public class BowlingGame
+{
+    public Bowler GetWinner()
+    {
+        return new Bowler("Jimit", 198);
+    }
+}
+
+public record Bowler (string Name, int Score);
+
+//public record Bowler
+//{
+//    public Bowler(string name, int score)
+//    {
+//        Name = name;
+//        Score = score;
+//    }
+//    public string Name { get; private set; }
+//    public int Score { get; private set; }
+//}
+
---
+++ CSharpSyntaxStuff/CreatingInstanceOfTypes.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSyntaxStuff;
+
+public class CreatingInstanceOfTypes
+{
+    [Fact]
+    public void DeterminingType()
+    {
+        Int32 x = 12;
+        int y = 12;
+
+        string myName = "Jimit";
+        String yourName = "Ringo";
+    }
+
+    [Fact]
+    public void ImplicitVariableDeclaration()
+    {
+        var x = 12;
+        var y = "Monkey";
+
+        var bob = new Employee();
+        var myPay = new PayCheck();
+        var manager = new Manager();
+        manager.SaveThis(bob);
+        manager.SaveThis(myPay);
+        manager.SaveThis("Tacos");
+        manager.SaveThis(42);
+    }
+
+    [Fact]
+    public void OldSchoolCollections()
+    {
+        var favoriteNumbers = new ArrayList();
+        favoriteNumbers.Add(9);
+        favoriteNumbers.Add(42);
+        favoriteNumbers.Add(20);
+        Assert.Equal(9, favoriteNumbers[0]);
+
+        favoriteNumbers.Add("Pizza");
+        // favoriteNumbers[0] = "Three";
+        var sumOfFirstTwo = ((int)favoriteNumbers[0]) + ((int)favoriteNumbers[1]);
+        Assert.Equal(51, sumOfFirstTwo);
+    }
+
+    [Fact]
+    public void NewSchoolCollections()
+    {
+        // Parametric Polymorphism
+        var favoriteNumbers = new List<int>();
+        favoriteNumbers.Add(9);
+        favoriteNumbers.Add(42);
+        favoriteNumbers.Add(20);
+        var sumOfFirstTwo = favoriteNumbers[0] + favoriteNumbers[1];
+        Assert.Equal(51, sumOfFirstTwo);
+    }
+}
+
+public class Employee { }
+
+public class PayCheck { }
+
+public class Manager
+{
+    public void SaveThis(Object o)
+    {
+
+    }
+}
