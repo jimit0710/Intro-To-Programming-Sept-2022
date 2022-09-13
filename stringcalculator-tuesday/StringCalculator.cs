8ca9832bbf9179aa7d284b7eab819fcd1110c538
Jimit Bhalavat <jimit_bhalavat@progressive.com>
2022-09-13 16:28:54 -0400

    StringCalculator

---
+++ stringcalculator-tuesday/StringCalculator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (String.IsNullOrEmpty(numbers))
+        {
+            return 0;
+        }
+
+        var result = numbers.Split(',')
+            .Select(s => int.Parse(s))
+            .Sum();
+        return result;
+    }
+}
---
+++ stringcalculator-tuesday/StringCalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+
+    private StringCalculator calculator = new StringCalculator();
+
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("132", 132)]
+    public void Length1ReturnsOne(string numbers, int expected)
+    {
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("2,4", 6)]
+    public void TwoNumbersReturnSum(string numbers, int expected)
+    {
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2,3,4", 10)]
+    [InlineData("2,4,8,16", 30)]
+    public void MulitpleNumbersReturnSum(string numbers, int expected)
+    {
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+}
+
