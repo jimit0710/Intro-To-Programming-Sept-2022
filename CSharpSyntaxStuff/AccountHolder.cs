a5f20102b0026a402fc2f4005ae70cf5d58eeda2
Jimit Bhalavat <jimit_bhalavat@progressive.com>
2022-09-14 12:16:46 -0400

    C#

---
+++ CSharpSyntaxStuff/AccountHolder.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CSharpSyntaxStuff;
+
+public class AccountHolder
+{
+       
+   public AccountHolder(string name, string accountNumber)
+    {
+        Name = name;
+        AccountNumber = accountNumber;
+    }
+
+    //public string Name
+    //{
+    //    get { return _name; }
+    //    set { _name = value; }
+    //}
+    // backing field
+    public string Name { get; set; } = string.Empty;
+
+    // "Auto Property"
+    public string Email { get; init; } = string.Empty;
+    
+    public string AccountNumber { get; private set; } = string.Empty; 
+
+    public string GetInfo()
+    {
+        return $"This Account Holder is {Name} and reach them at {Email}";
+    }
+}
+
+public class Dog
+{
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public void RollOver()
+    {
+
+    }
+}
---
+++ CSharpSyntaxStuff/AccountHolderTests.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharpSyntaxStuff;
+
+public class AccountHolderTests
+{
+    [Fact]
+    public void AccountHolderStuff()
+    {
+        var ah = new AccountHolder("Bob Smith", "xyz-pdq");
+        var ah2 = new AccountHolder("Jill Jones", "9999") { Email = "Jill@aol.com" };
+
+        // ah.Email = "Bob@aol.com";
+
+        var name = ah.Name;
+        // ah.Name = "Bob Smith";
+        Assert.Equal("Bob Smith", ah.Name);
+
+        // Assert.Equal("This Account Holder is Bob Smith", ah.GetInfo());
+
+        var rover = new Dog()
+        {
+            Name = "Rover",
+            Breed = "Terrier"
+        };
+        rover.RollOver();
+    }
+}
