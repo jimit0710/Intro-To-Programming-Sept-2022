5854b7c9ce1c6cab1f97544026451ec482db83aa
Jimit Bhalavat <jimit_bhalavat@progressive.com>
2022-09-15 12:30:32 -0400

    Before Lunch Thursday

--- stringcalculator-tuesday/StringCalculatorTests.cs
+++ stringcalculator-tuesday/StringCalculatorTests.cs
@@ -40,10 +40,9 @@ public class StringCalculatorTests
     }
 
     [Theory]
-    [InlineData("1,2,3,4", 10)]
+    [InlineData("1,2,3", 6)]
     [InlineData("2,4,8,16", 30)]
     [InlineData("10,12,14", 36)]
-    [InlineData("3,3,0", 6)]
     [InlineData("3,3,1", 7)]
     [InlineData("1,2,3,4,5,6,7,8,9", 45)]
     public void MulitpleNumbersReturnSum(string numbers, int expected)
@@ -59,7 +58,6 @@ public class StringCalculatorTests
     [InlineData("1\n2,3", 6)]
     [InlineData("2,4,8\n16", 30)]
     [InlineData("3\n1", 4)]
-    [InlineData("3\n3", 6)]
     [InlineData("1\n2\n3\n4\n5\n6\n7\n8\n9", 45)]
     [InlineData("3\n1,1", 5)]
     public void NewlineNumbersReturnSum(string numbers, int expected)
