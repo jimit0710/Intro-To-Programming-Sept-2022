b6844a61b4186726743f4e3c8f292a373089c62a
Jimit Bhalavat <jimit_bhalavat@progressive.com>
2022-09-20 14:38:06 -0400

    Angular

---
+++ playlists/src/app/features/playlists/components/song-list/song-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-song-list',
+  templateUrl: './song-list.component.html',
+  styleUrls: ['./song-list.component.css'],
+})
+export class SongListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
